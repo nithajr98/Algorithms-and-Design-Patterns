d42ed7885581eb4d5973dddb1626b94277f135f6
Nithaj R <nithaj.r@NetCracker.com>
2024-04-30 19:06:38 +0530

    dfs start

--- DesignPatterns/src/Algorithms/DFS.java
+++ DesignPatterns/src/Algorithms/DFS.java
@@ -1,6 +1,24 @@
 package Algorithms;
 
-public class DFS {
+import java.util.LinkedList;
 
+public class DFS {
+    int v;
+    LinkedList<Integer>[] adj;
+    DFS(int v){
+        this.v=v;
+        for(int i=0;i<v;i++){
+            adj[i]=new LinkedList<>();
+        }
+    }
+
+    void DFSmain(int start){
+        boolean[] visited = new boolean[v];
+
+    }
+
+    void DFSUtil(int v,boolean[] visited){
+
+    }
 
 }
